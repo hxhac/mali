3439969df849005d8a54b56daccaed1accae9340
x1a0xf <yyzw@live.com>
2022-04-26 00:34:21 +0800

    rm: 移除之前的goods模块代码

--- server/service/enter.go
+++ server/service/enter.go
@@ -3,14 +3,12 @@ package service
 import (
 	"github.com/flipped-aurora/gin-vue-admin/server/service/daily"
 	"github.com/flipped-aurora/gin-vue-admin/server/service/example"
-	"github.com/flipped-aurora/gin-vue-admin/server/service/goods"
 	"github.com/flipped-aurora/gin-vue-admin/server/service/system"
 )
 
 type ServiceGroup struct {
 	SystemServiceGroup  system.ServiceGroup
 	ExampleServiceGroup example.ServiceGroup
-	GoodsServiceGroup   goods.ServiceGroup
 	DailyServiceGroup   daily.ServiceGroup
 }
 
