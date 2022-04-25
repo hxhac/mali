d91aa379043c0e9b2885731058fddb9ef995335c
x1a0xf <yyzw@live.com>
2022-04-26 00:34:21 +0800

    rm: 移除之前的goods模块代码

--- server/api/v1/enter.go
+++ server/api/v1/enter.go
@@ -3,14 +3,12 @@ package v1
 import (
 	"github.com/flipped-aurora/gin-vue-admin/server/api/v1/daily"
 	"github.com/flipped-aurora/gin-vue-admin/server/api/v1/example"
-	"github.com/flipped-aurora/gin-vue-admin/server/api/v1/goods"
 	"github.com/flipped-aurora/gin-vue-admin/server/api/v1/system"
 )
 
 type ApiGroup struct {
 	SystemApiGroup  system.ApiGroup
 	ExampleApiGroup example.ApiGroup
-	GoodsApiGroup   goods.ApiGroup
 	DailyApiGroup   daily.ApiGroup
 }
 
