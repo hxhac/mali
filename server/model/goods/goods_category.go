df05dc12591927504acd1bbff21558c386a46ed9
x1a0xf <yyzw@live.com>
2022-06-03 12:15:25 +0800

    feat(goods): 给goods_category表添加待购清单字段is_wish_list

--- server/model/goods/goods_category.go
+++ server/model/goods/goods_category.go
@@ -9,10 +9,11 @@ import (
 // 如果含有time.Time 请自行import time包
 type GoodsCategory struct {
 	global.GVA_MODEL
-	Pid      *int            `json:"pid" form:"pid" gorm:"column:pid;comment:;size:10;"`
-	CateName string          `json:"cateName" form:"cateName" gorm:"column:cate_name;comment:分类名称;size:255;unique;"`
-	More     string          `json:"more" form:"more" gorm:"column:more;comment:备注;"`
-	Children []GoodsCategory `json:"children" gorm:"-"`
+	Pid        *int            `json:"pid" form:"pid" gorm:"column:pid;comment:;size:10;"`
+	CateName   string          `json:"cateName" form:"cateName" gorm:"column:cate_name;comment:分类名称;size:255;unique;"`
+	More       string          `json:"more" form:"more" gorm:"column:more;comment:备注;"`
+	IsWishList *bool           `json:"isWishList" form:"isWishList" gorm:"is_wish_list;comment:是否在待购清单;default:0;not null;"`
+	Children   []GoodsCategory `json:"children" gorm:"-"`
 }
 
 // TableName GoodsCategory 表名
