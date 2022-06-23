00412d7d243cc4f89770047a9117b5204f114d63
x1a0xf <yyzw@live.com>
2022-06-23 09:58:30 +0800

    feed: 给feed添加“说明”字段
    alter table rss_feed
        add remark varchar(255) null;

--- server/model/rss/rss_feed.go
+++ server/model/rss/rss_feed.go
@@ -13,6 +13,7 @@ type RssFeed struct {
 	CateId      *int        `json:"cateId" form:"cateId" gorm:"column:cate_id;comment:rss分类;size:10;"`
 	Url         string      `json:"url" form:"url" gorm:"column:url;comment:rss的url;size:255;"`
 	Keywords    string      `json:"keywords" form:"keywords" gorm:"column:keywords;comment:关键字过滤，逗号连接;size:255;"`
+	Remark      string      `json:"remark" form:"remark" gorm:"column:remark;comment:备注;size:255;"`
 	IsPause     *bool       `json:"isPause" form:"isPause" gorm:"column:is_pause;comment:是否暂停（0不暂停1暂停）;default:0"`
 	IsStarred   *bool       `json:"isStarred" form:"isStarred" gorm:"column:is_starred;comment:是否收藏;default:0;not null;"`
 	RssCategory RssCategory `json:"rss_category" gorm:"foreignKey:CateId"`
