75d311b3c6a967915451bbcdc22762388ef2715b
x1a0xf <yyzw@live.com>
2022-06-23 13:14:03 +0800

    feat(rss分类): 优化排序

--- server/service/rss/rss_category.go
+++ server/service/rss/rss_category.go
@@ -87,6 +87,6 @@ from rss_category as rc
          left join rss_feed as rf on rc.id = rf.cate_id
 where rc.deleted_at is null
 group by rc.id
-order by num desc;`).Limit(limit).Offset(offset).Find(&rssCategorys).Error
+order by rc.is_update desc, rc.num desc, rc.is_mute asc;`).Limit(limit).Offset(offset).Find(&rssCategorys).Error
 	return err, rssCategorys, total
 }
