7941ad8e9cfa32175c4a10ad1628a25abfc0f057
x1a0xf <yyzw@live.com>
2023-02-14 20:09:57 +0800

    feat: 完善zap日志，并添加dc挂载 #141 #142
    [zap | Gin-Vue-Admin](https://www.gin-vue-admin.com/guide/server/core/zap.html)

--- server/main.go
+++ server/main.go
@@ -4,7 +4,6 @@ import (
 	"github.com/flipped-aurora/gin-vue-admin/server/core"
 	"github.com/flipped-aurora/gin-vue-admin/server/global"
 	"github.com/flipped-aurora/gin-vue-admin/server/initialize"
-	"github.com/sirupsen/logrus"
 	"go.uber.org/zap"
 )
 
@@ -12,8 +11,6 @@ import (
 //go:generate go env -w GOPROXY=https://goproxy.cn,direct
 //go:generate go mod tidy
 //go:generate go mod download
-
-var logger = logrus.New()
 
 // @title Swagger Example API
 // @version 0.0.1
